1f715be45a7191eaec129660f31d38e75cf14454
Abdulnasir-w <Abdulnasir-w@users.noreply.github.com>
2024-02-21 00:07:08 +0500

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:to_do_list/Screens/Screens/edit_todo_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
