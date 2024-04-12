0b6159887cc0e6270e5ebb08453a5a548b9d6d0c
Abdulnasir-w <Abdulnasir-w@users.noreply.github.com>
2024-04-12 23:44:56 +0500

    Initial

--- lib/Components/custom_detail_appbar.dart
+++ lib/Components/custom_detail_appbar.dart
@@ -22,73 +22,62 @@ class _DetailsScreenAppbarState extends State<DetailsScreenAppbar> {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.transparent,
-      leading: Container(
-        width: 24,
-        height: 24,
-        child: SvgPicture.asset(
-          "assets/Icons/back.svg",
-          width: 24,
-          height: 24,
-        ),
-      ),
-      leadingWidth: 160,
+      automaticallyImplyLeading: true,
       title: Padding(
         padding: const EdgeInsets.only(
-          left: 130.0,
+          left: 230.0,
         ),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              InkWell(
-                onTap: () {
-                  Deadline.showMenuFun(context, widget.taskId);
-                },
-                child: SvgPicture.asset(
-                  "assets/Icons/clock.svg",
-                  width: 50,
-                  height: 24,
-                ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                Deadline.showMenuFun(context, widget.taskId);
+              },
+              child: SvgPicture.asset(
+                "assets/Icons/clock.svg",
+                width: 50,
+                height: 24,
+                fit: BoxFit.fill,
               ),
-              const SizedBox(
-                width: 10,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => EditTodoScreen(
+                            taskId: widget.taskId,
+                          )),
+                );
+              },
+              child: SvgPicture.asset(
+                "assets/Icons/edit.svg",
+                width: 50,
+                height: 24,
               ),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => EditTodoScreen(
-                              taskId: widget.taskId,
-                            )),
-                  );
-                },
-                child: SvgPicture.asset(
-                  "assets/Icons/edit.svg",
-                  width: 50,
-                  height: 24,
-                ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DeleteTodoScreen()),
+                );
+              },
+              child: SvgPicture.asset(
+                "assets/Icons/trash.svg",
+                width: 50,
+                height: 24,
               ),
-              const SizedBox(
-                width: 10,
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const DeleteTodoScreen()),
-                  );
-                },
-                child: SvgPicture.asset(
-                  "assets/Icons/trash.svg",
-                  width: 50,
-                  height: 24,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
