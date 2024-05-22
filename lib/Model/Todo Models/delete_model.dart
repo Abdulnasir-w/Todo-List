218ce7af36b0266f4eb31042caa837dfb1fb3601
Abdulnasir-w <Abdulnasir-w@users.noreply.github.com>
2024-05-22 16:01:59 +0500

    Initial

--- lib/Model/Todo Models/delete_model.dart
+++ lib/Model/Todo Models/delete_model.dart
@@ -1,13 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 Future<void> deleteTodoItem(String id) async {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
   try {
-    print("$id");
-    await FirebaseFirestore.instance.collection("Tasks").doc(id).delete();
-
-    Fluttertoast.showToast(msg: "Deleted Successfully.");
+    User? user = auth.currentUser;
+    if (user != null) {
+      CollectionReference<Map<String, dynamic>> userTasks =
+          firestore.collection("users").doc(user.uid).collection("Tasks");
+      await userTasks.doc(id).delete();
+      Fluttertoast.showToast(msg: "Successfully Deleted");
+    } else {
+      Fluttertoast.showToast(msg: "User Not Found");
+    }
   } catch (e) {
-    throw Exception(e);
+    Fluttertoast.showToast(msg: "$e");
   }
 }
