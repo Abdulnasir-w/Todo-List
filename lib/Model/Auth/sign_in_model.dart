bfd4e67f4747013c80b0be70d441c184b92846a6
Abdul Nasir <abdulnasir9323@gmail.com>
2024-12-31 16:13:53 +0500

    Updated and also remove deprecated things

--- lib/Model/Auth/sign_in_model.dart
+++ lib/Model/Auth/sign_in_model.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,11 +19,39 @@ class SignIn {
       // Save The Token Locally
       await saveTokenLocally(userCredential.user!.uid);
       return userCredential;
+    } on FirebaseAuthException catch (e) {
+      String errorMessage = "An error occurred during sign in";
+
+      switch (e.code) {
+        case 'invalid-credential':
+          errorMessage = "Invalid email or password";
+          break;
+        case 'user-disabled':
+          errorMessage = "This account has been disabled";
+          break;
+        case 'user-not-found':
+          errorMessage = "No account found with this email";
+          break;
+        case 'wrong-password':
+          errorMessage = "Incorrect password";
+          break;
+        case 'invalid-email':
+          errorMessage = "Invalid email address";
+          break;
+      }
+
+      Fluttertoast.showToast(
+        msg: errorMessage,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
     } catch (e) {
       Fluttertoast.showToast(
           msg: "Cannot Login Email or Password is Incorrect");
       rethrow;
     }
+    // Add a throw statement to ensure a return value
+    throw Exception("Sign in failed");
   }
 
   // Save the authentication token locally
